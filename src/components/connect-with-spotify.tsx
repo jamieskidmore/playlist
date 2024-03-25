a15e62fc2d0d24f786af9b7d1c2872d27e5788b4
Jamie <jskidmore9@my.bcit.ca>
2024-03-25 10:33:57 -0700

    test

--- src/components/connect-with-spotify.tsx
+++ src/components/connect-with-spotify.tsx
@@ -1,4 +1,4 @@
-export default function ConnectToSpotify() {
+export default function ConnectWithSpotify() {
   const client_id = "b6b24206aae34ef488bb7833fa7e718f"; // This should be an environment variable
   const redirect_uri = "http://localhost:3000/callback";
   const scope = encodeURIComponent(
@@ -9,7 +9,7 @@ export default function ConnectToSpotify() {
     <a
       href={`https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}`}
     >
-      <button>Connect with Spotify</button>
+      <button>Connect with Spotify!</button>
     </a>
   );
 }
