8d0f06d4586ceed21aeaee8f0eda7157bf3292f9
Jamie <jskidmore9@my.bcit.ca>
2024-04-14 06:38:25 -0700

    fix errors

--- src/components/connect-with-apple.tsx
+++ src/components/connect-with-apple.tsx
@@ -1,11 +1,16 @@
-"use client";
-
-import Link from "next/link";
 import ConnectToApiButton from "./connect-to-api-button";
+
+const client_id = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
+const redirect_uri = process.env.REDIRECT_URI;
+const scope = encodeURIComponent(
+  "playlist-modify-public playlist-modify-private"
+);
 
 export default function ConnectWithApple() {
   return (
-    <a href="{`https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}`}">
+    <a
+      href={`https://accounts.spotify.com/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}`}
+    >
       <ConnectToApiButton service="Apple" />
     </a>
   );
