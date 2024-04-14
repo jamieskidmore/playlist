547e35c825e8fadb32837d1b09f4baf8606fdc17
Jamie <jskidmore9@my.bcit.ca>
2024-04-14 13:18:38 -0700

    check apple playlist data

--- src/components/create-apple-playlist.tsx
+++ src/components/create-apple-playlist.tsx
@@ -57,9 +57,20 @@ export default function CreateApplePlaylist({
           headers: { Authorization: `Bearer ${spotifyAccessToken}` },
         });
         const playlistData = await playlistResponse.json();
+        console.log(1);
         console.log(playlistData);
+        console.log(2);
         console.log(playlistData.tracks);
-        console.log(playlistData[0].tracks);
+        console.log(3);
+        console.log(playlistData.tracks.items);
+        console.log(4);
+        console.log(playlistData.tracks.json());
+        console.log(5);
+        console.log(playlistData.tracks.items.json());
+        console.log(6);
+        console.log(playlistData.tracks.items[0].json());
+        console.log(7);
+        console.log(playlistData.tracks[0].items);
         // const tracks = playlistData.data[0].relationships.tracks.data.map(
         //   (track: {
         //     attributes: { name: string; artistName: string; albumName: string };
