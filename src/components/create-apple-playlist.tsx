14f762630492bf32dc4441c7b91c40fa80efab40
Jamie <jskidmore9@my.bcit.ca>
2024-04-14 06:59:08 -0700

    checks

--- src/components/create-apple-playlist.tsx
+++ src/components/create-apple-playlist.tsx
@@ -44,7 +44,7 @@ export default function CreateApplePlaylist({
     };
 
     if (!localStorage.getItem("appleUserToken")) {
-      console.log("here");
+      console.log(window.MusicKit);
       handleConnectWithApple();
     }
   }, [appleDeveloperToken]);
