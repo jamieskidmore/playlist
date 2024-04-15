1965a5dc2c39072d2c49062c7ebcc6b781d6c3d5
Jamie <jskidmore9@my.bcit.ca>
2024-04-14 21:39:28 -0700

    get songs data

--- src/components/create-apple-playlist.tsx
+++ src/components/create-apple-playlist.tsx
@@ -82,7 +82,7 @@ export default function CreateApplePlaylist({
 
           if (searchResponse) {
             const search = await searchResponse.json();
-            console.log(search.results);
+            console.log(search.results.songs[0].href);
           } else {
             console.log("Not found: " + track.name);
           }
