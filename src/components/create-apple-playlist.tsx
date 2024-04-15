b95a16c046cb55065d3e4fa20a27ceec32b1fac5
Jamie <jskidmore9@my.bcit.ca>
2024-04-15 00:15:04 -0700

    test

--- src/components/create-apple-playlist.tsx
+++ src/components/create-apple-playlist.tsx
@@ -221,7 +221,7 @@ export default function CreateApplePlaylist({
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
-      console.log(response);
+      console.log(response.body);
     } catch (error) {
       console.error("Failed to get url", error);
     }
